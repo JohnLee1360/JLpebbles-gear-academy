c5e8995cb334c3947a4a41eaaf9a848c85cce37d
JohnLee1360 <1055341372@qq.com>
2024-07-18 13:00:59 +0800

    pebbles game assignment

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+#![no_std]
+
+use gstd::{};
+use pebbles_game_io::*;
+
+static  mut PEBBLES_GAME: Option<GameState> = None;
+
+#[no_mangle]
+extern "C" fn init() {
+
+}
+
+extern "C" fn handle() {
+
+}
+
+extern "C" fn state() {
+
+}
